65ce4b3f5811d3108c63807c3f50bd7491c8e080
re8423 <maina8423@gmail.com>
2024-04-10 19:34:42 +0100

    bash

--- number_crunching_likwid_all.sh
+++ number_crunching_likwid_all.sh
@@ -11,6 +11,12 @@ module load gcc/12.2 likwid/5.2.0
 g++ -mfma -O1 -DLIKWID_PERFMON -fno-inline -march=native -o num_crunch_likwid_all number_crunching_likwid_all.cpp -llikwid
 
 likwid-perfctr -m -g "MEM_DP" -C 0 ./num_crunch_likwid_all 10000
+likwid-perfctr -m -g "MEM_DP" -C 0 ./num_crunch_likwid_all 20000
+likwid-perfctr -m -g "MEM_DP" -C 0 ./num_crunch_likwid_all 30000
 likwid-perfctr -m -g "MEM_DP" -C 0 ./num_crunch_likwid_all 40000
+likwid-perfctr -m -g "MEM_DP" -C 0 ./num_crunch_likwid_all 50000
+likwid-perfctr -m -g "MEM_DP" -C 0 ./num_crunch_likwid_all 60000
 likwid-perfctr -m -g "MEM_DP" -C 0 ./num_crunch_likwid_all 70000
+likwid-perfctr -m -g "MEM_DP" -C 0 ./num_crunch_likwid_all 80000
+likwid-perfctr -m -g "MEM_DP" -C 0 ./num_crunch_likwid_all 90000
 likwid-perfctr -m -g "MEM_DP" -C 0 ./num_crunch_likwid_all 100000
