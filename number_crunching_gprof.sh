dee872ae63cb861cea7121c50927d408353f5b7d
re8423 <maina8423@gmail.com>
2024-04-10 17:55:18 +0100

    bash

--- number_crunching_gprof.sh
+++ number_crunching_gprof.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 #SBATCH -p test
+#SBATCH --mem=1G
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=00:30:00
+#SBATCH --time=00:35:00
+
+./number_crunching 10000
+gprof -zb number_crunching gmon.out
+
+./number_crunching 20000
+gprof -zb number_crunching gmon.out
+
+./number_crunching 30000
+gprof -zb number_crunching gmon.out
 
 ./number_crunching 40000
 gprof -zb number_crunching gmon.out
 
-# gprof -zb number_crunching gmon.out > number_crunching_gprof.out
+./number_crunching 50000
+gprof -zb number_crunching gmon.out
+
+./number_crunching 60000
+gprof -zb number_crunching gmon.out
+
+./number_crunching 70000
+gprof -zb number_crunching gmon.out
+
+./number_crunching 80000
+gprof -zb number_crunching gmon.out
+
+./number_crunching 90000
+gprof -zb number_crunching gmon.out
+
+./number_crunching 100000
+gprof -zb number_crunching gmon.out
+
+gprof -zb number_crunching gmon.out > number_crunching_gprof.out
--- number_crunching_likwid.sh
+++ number_crunching_likwid.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH -p test
+#SBATCH --mem=1G
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=00:30:00
+#SBATCH --time=00:10:00
 
 likwid-perfctr -m -g "MEM_DP" -C 0 ./num_crunch_likwid 40000
 likwid-perfctr -m -g "FLOPS_DP" -C 0 ./num_crunch_likwid 40000
