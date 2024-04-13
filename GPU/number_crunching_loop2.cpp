96f6ebe39ebd301708d9ef13be51cf1f105cc4e4
re8423 <maina8423@gmail.com>
2024-04-13 20:40:22 +0100

    bash

--- GPU/number_crunching_loop2.cpp
+++ GPU/number_crunching_loop2.cpp
@@ -32,14 +32,24 @@ void function_b(double *x, const double a, const double *u, const double *v, con
 void function_c(double *z, const double s, const double *x, const double *y,
                    const int N) {
   
+  // #pragma omp target teams distribute parallel for map(to:s, x[0:N], y[0:N]) map(from:z[0:N]) 
+  // for (unsigned int i = 0; i < N; i++) {
+  //   if (i % 2 == 0) {
+  //     z[i] = s * x[i] + y[i];
+  //   } else {
+  //     z[i] = x[i] + y[i];
+  //   }
+  // }
   #pragma omp target teams distribute parallel for map(to:s, x[0:N], y[0:N]) map(from:z[0:N]) 
-  for (unsigned int i = 0; i < N; i++) {
-    if (i % 2 == 0) {
+  for (unsigned int i = 0; i < N; i=i+2) {
       z[i] = s * x[i] + y[i];
-    } else {
-      z[i] = x[i] + y[i];
-    }
   }
+
+  #pragma omp target teams distribute parallel for map(to:s, x[0:N], y[0:N]) map(from:z[0:N]) 
+  for (unsigned int i = 1; i < N; i=i+2) {
+      z[i] = x[i] + y[i];
+  }
+
 }
 
 double function_d(double s, const double *u, const double *v, const int N) {
