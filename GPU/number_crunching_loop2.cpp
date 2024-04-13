4d2f6e29cbfeb9632dc23472f938b25125d9959e
re8423 <maina8423@gmail.com>
2024-04-13 20:33:27 +0100

    bash

--- GPU/number_crunching_loop2.cpp
+++ GPU/number_crunching_loop2.cpp
@@ -22,7 +22,7 @@ void function_a(double *y, const double *A, const double *x, const int N) {
 void function_b(double *x, const double a, const double *u, const double *v, const int N) {
   
   // instead of tofrom, shouldnt from be better?
-  #pragma omp target teams distribute parallel for map(to:a, u[0:N], v[0:N]) map(tofrom:x[0:N])
+  #pragma omp target teams distribute parallel for map(to:a, u[0:N], v[0:N]) map(from:x[0:N])
   for (unsigned int i = 0; i < N; i++) {
     x[i] = a * u[i] + v[i];
   }
@@ -32,7 +32,7 @@ void function_b(double *x, const double a, const double *u, const double *v, con
 void function_c(double *z, const double s, const double *x, const double *y,
                    const int N) {
   
-  #pragma omp target teams distribute parallel for map(to:s, x[0:N], y[0:N]) map(tofrom:z[0:N]) 
+  #pragma omp target teams distribute parallel for map(to:s, x[0:N], y[0:N]) map(from:z[0:N]) 
   for (unsigned int i = 0; i < N; i++) {
     if (i % 2 == 0) {
       z[i] = s * x[i] + y[i];
