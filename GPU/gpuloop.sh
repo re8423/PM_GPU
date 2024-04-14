2c1f93c25c068f06f82474e86e56265114303264
re8423 <maina8423@gmail.com>
2024-04-14 18:29:41 +0100

    bash

--- GPU/gpuloop.sh
+++ GPU/gpuloop.sh
@@ -3,13 +3,12 @@
 #SBATCH -N 1
 #SBATCH -c 4
 #SBATCH --time=00:15:00
-#SBATCH --gres=gpu:1g.10gb:1
-#SBATCH --mem=20G	# memory required, up to 250G on standard nodes
+#SBATCH --gres=gpu:turing:1
 source /etc/profile
 
 module load nvidia-hpc
 nvc++ -fopenmp -mp=gpu -o loopgpu2 number_crunching_loop2.cpp
-OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 time ./loopgpu2 100000
+OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 time ./loopgpu2 50000
 
 # OMP_NUM_THREADS=50 time ./loopgpu2 90000
 
