48a6a2e12f485c22806b6a08196d983fb7c250e5
re8423 <maina8423@gmail.com>
2024-04-14 20:57:19 +0100

    bash

---
+++ GPU/correctness.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+module load nvidia-hpc
+nvc++ -fopenmp -mp=gpu -o loopgpu2 number_crunching_loop2.cpp
+
+OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 ./loopgpu2 100000
+./serial 100000
+diff serial_results.out partial_results.out
+
+
+OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 ./loopgpu2 123456
+./serial 123456
+diff serial_results.out partial_results.out
+
+OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 ./loopgpu2 78345
+./serial 78345
+diff serial_results.out partial_results.out
+
+OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 ./loopgpu2 5
+./serial 5
+diff serial_results.out partial_results.out
+
+OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 ./loopgpu2 1002
+./serial 1002
+diff serial_results.out partial_results.out
+
+OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 ./loopgpu2 89456
+./serial 89456
+diff serial_results.out partial_results.out
