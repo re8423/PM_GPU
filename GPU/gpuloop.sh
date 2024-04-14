8cf9a9062a67b99881f40a79813626ba1061c35d
re8423 <maina8423@gmail.com>
2024-04-14 17:58:42 +0100

    bash

--- GPU/gpuloop.sh
+++ GPU/gpuloop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -p tpg-gpu-small
 #SBATCH --mem-per-cpu=20G
-#SBATCH -N 10		# number of compute nodes. 
-#SBATCH -c 32		# number of CPU cores, one per thread, up to 128
+#SBATCH -N 1		# number of compute nodes. 
+#SBATCH -c 10		# number of CPU cores, one per thread, up to 128
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:15:00
 
@@ -25,4 +25,4 @@ nvc++ -fopenmp -mp=gpu -o loopgpu number_crunching_loop.cpp
 
 # OMP_NUM_THREADS=50 time ./loopgpu 20000
 
-OMP_NUM_THREADS=32 time ./loopgpu 10000
+OMP_NUM_THREADS=10 time ./loopgpu 10000
