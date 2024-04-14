3e4aa39e40422acd3c8b50b91459061486c936b3
re8423 <maina8423@gmail.com>
2024-04-14 18:15:35 +0100

    bash

--- GPU/gpuloop.sh
+++ GPU/gpuloop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH -p tpg-gpu-small
-#SBATCH -N 1
-#SBATCH -c 1
+#SBATCH -N 10
+#SBATCH -c 10
 #SBATCH --gres=gpu
 #SBATCH --time=00:15:00
 source /etc/profile
 
 module load nvidia-hpc
 nvc++ -fopenmp -mp=gpu -o loopgpu2 number_crunching_loop2.cpp
-OMP_NUM_THREADS=1 time ./loopgpu2 100000
+OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 time ./loopgpu2 100000
 
 # OMP_NUM_THREADS=50 time ./loopgpu2 90000
 
