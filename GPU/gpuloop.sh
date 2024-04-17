cc7d66205d36d4cdbf870a8566dd02c680e1c113
re8423 <maina8423@gmail.com>
2024-04-17 16:19:38 +0100

    bash

--- GPU/gpuloop.sh
+++ GPU/gpuloop.sh
@@ -8,7 +8,7 @@ source /etc/profile
 
 module load nvidia-hpc
 nvc++ -fopenmp -mp=gpu -o loopgpu2 number_crunching_loop2.cpp
-OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 time ./loopgpu2 123456
+OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 time ./loopgpu2 5000
 # OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 time ./loopgpu2 90000
 # OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 time ./loopgpu2 80000
 # OMP_NUM_TEAMS=10 OMP_NUM_THREADS=10 time ./loopgpu2 70000
